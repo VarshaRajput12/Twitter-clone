623d0db099edb67dfa7f566d4816d3a21901e747
VarshaRajput12 <varsharajput353@gmail.com>
2022-12-31 23:17:01 +0530

    updated

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const loginPage = document.querySelector(".login_page");
 const middleContent = document.querySelector(".middle_content");
 const btnTop = document.querySelector(".top_btn");
 const newsFeedPage = document.querySelector(".feeds_page");
+const loginModal = document.querySelector(".login_modal");
+const modalLX = document.querySelector(".login_modal i");
+const loginFormBtn = document.querySelector(".login_form_btn");
 
 
 
@@ -21,13 +24,36 @@ middleContent.addEventListener("click", (e)=>{
 
 btnTop.addEventListener("click", ()=>{
     const inputUserInfo = document.querySelector(".user_info");
-    const inputPassword = document.querySelector(".user_info");
+    const inputPassword = document.querySelector(".password");
      
     if(inputUserInfo.value !== "" && inputPassword.value !== ""){
         mainPage.style.display = "none"
         newsFeedPage.style.display = "block"
     }else{
         goToLoginPage()
+        loginModal.style.display = "block"
+    }
+
+})
+
+// login page
+modalLX.addEventListener("click", ()=> {
+    loginModal.style.display = "none"
+})
+
+loginFormBtn.addEventListener("click",)
+
+
+loginFormBtn.addEventListener("click", ()=>{
+    const login_user_info = document.querySelector(".login_user_info");
+    const login_password = document.querySelector(".login_password");
+     
+    if(login_user_info.value !== "" && login_password.value !== ""){
+        loginPage.style.display = "none"
+        newsFeedPage.style.display = "block"
+    }else{
+        goToLoginPage()
+        loginModal.style.display = "block"
     }
 
 })
